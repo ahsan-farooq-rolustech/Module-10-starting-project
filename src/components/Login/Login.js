81e616405ecdab58bcdc1f243602e1057ccc6711
Muhammad Ahsan Farooq <ahsan.farooq@rolustech.net>
2022-10-06 14:38:27 +0500

    used useReducer instead of useState for the password state in Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -39,8 +39,8 @@ const passwordReducer=(state,action)=>{
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {value:'',isValid:false})
@@ -72,7 +72,7 @@ const Login = (props) => {
     dispatchEmail({type:'USER_INPUT',val:event.target.value})
 
     setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
+      event.target.value.includes('@') && passwordState.isValid
     );
   };
 
